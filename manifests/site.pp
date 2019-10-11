e1b0da98ebd6a9d0675c1bfff9b9f53d3fb1745e
Johan <johasel@stud.ntnu.no>
2019-10-11 19:08:12 +0200

    small changes to site.pp to get puppetdb working

--- manifests/site.pp
+++ manifests/site.pp
@@ -12,8 +12,7 @@ node /lin\d?.node.consul/ {
 node 'manager.node.consul' {
   include ::role::manager
 
-  # Here we configure the Puppet master to use PuppetDB,
-  # telling it the hostname of the PuppetDB node
+  # Tell Puppet master to use PuppetDB and hostname of the PuppetDB node
   class { 'puppetdb::master::config':
   puppetdb_server => $puppetdb_host,
   }
@@ -24,15 +23,13 @@ node 'mon.node.consul'{
   # Here we install and configure PostgreSQL and the PuppetDB
   # database instance, and tell PostgreSQL that it should
   # listen for connections to the `$postgres_host`
-  class { 'puppetdb::database::postgresql':
+  class { 'puppetdb':
   listen_addresses => $postgres_host,
   }
 
   # Here we install and configure PuppetDB, and tell it where to
   # find the PostgreSQL database.
-  class { 'puppetdb::server':
-  database_host => $postgres_host,
-  }
+
 }
 node 'dir.node.consul' {
   include ::role::directory_server
