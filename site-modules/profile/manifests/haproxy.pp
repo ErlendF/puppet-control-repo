ac713e518e91f2f2c836a9731f08db788b9a3ce4
Aksel Baardsen <akselba@stud.ntnu.no>
2019-10-15 20:06:35 +0200

    added webserver nodes to loadbalancer

--- site-modules/profile/manifests/haproxy.pp
+++ site-modules/profile/manifests/haproxy.pp
@@ -4,16 +4,34 @@
 #
 class profile::haproxy {
     include ::haproxy
+    haproxy::listen { 'forwarder':
+      collect_exported => false,
+      ipaddress        => '*',
+      ports            => '5000',
+    }
+    haproxy::balancermember { 'puppetserver':
+      listening_service => 'forwarder',
+      ipaddresses       => 'puppetdb.node.consul',
+      ports             => '5000',
+      options           => 'check',
+    } 
+
     haproxy::listen { 'balancer':
       collect_exported => false,
       ipaddress        => '*',
       ports            => '80',
     }
-    haproxy::balancermember { 'puppetserver':
+    haproxy::balancermember { 'lin0':
       listening_service => 'balancer',
-      ipaddresses       => 'puppetdb.node.consul',            # get from consul for dynamic(?)
-      ports             => '5000',
+      ipaddresses       => 'lin0.node.consul',
+      ports             => '8080',
       options           => 'check',
-    } 
+    }
+    haproxy::balancermember { 'lin1':
+      listening_service => 'balancer',
+      ipaddresses       => 'lin1.node.consul',
+      ports             => '8080',
+      options           => 'check',
+    }
 }
 
