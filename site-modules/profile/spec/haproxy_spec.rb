f5fe815b3b7bfe28b1bcb50805e03ca914bc5374
Aksel Baardsen <akselba@stud.ntnu.no>
2019-10-17 13:54:16 +0200

    removed reoccuring 'describe'-block

--- site-modules/profile/spec/haproxy_spec.rb
+++ site-modules/profile/spec/haproxy_spec.rb
@@ -2,13 +2,12 @@ require 'spec_helper'
 
 describe 'profile::haproxy' do 
 
-    describe 'should include ::haproxy' do
 
-        it { is_expected.to contain__haproxy('') }
-
-        it { is_expected.to compile }
-        it { is_expected.to compile.with_all_deps }
-
+    it 'should include ::haproxy' do
+        is_expected.to contain__haproxy('') 
     end
+
+    it { is_expected.to compile }
+    it { is_expected.to compile.with_all_deps }
 
 end
