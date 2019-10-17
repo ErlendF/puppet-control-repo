cd0e82e81d60229f1e29e7bf0392e7299fcf554c
Aksel Baardsen <akselba@stud.ntnu.no>
2019-10-17 13:48:09 +0200

    changed first 'it'-block to 'describe'

---
+++ site-modules/profile/spec/haproxy_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe 'profile::haproxy' do 
+
+    describe 'should include ::haproxy' do
+
+        it { is_expected.to contain__haproxy('') }
+
+        it { is_expected.to compile }
+        it { is_expected.to compile.with_all_deps }
+
+    end
+
+end
