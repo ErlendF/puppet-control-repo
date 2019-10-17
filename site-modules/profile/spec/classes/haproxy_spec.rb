b11e4c3eeaef411740ab2763d8a46b976604795c
Aksel Baardsen <akselba@stud.ntnu.no>
2019-10-17 14:06:32 +0200

    moved unit test to right dir

--- site-modules/profile/spec/classes/haproxy_spec.rb
+++ site-modules/profile/spec/classes/haproxy_spec.rb
@@ -1,14 +1,12 @@
 require 'spec_helper'
 
-describe 'profile::haproxy' do 
+describe 'profile::haproxy', :type => :class do 
 
     it 'should include ::haproxy' do
-
-        it { is_expected.to contain__haproxy('') }
-
-        it { is_expected.to compile }
-        it { is_expected.to compile.with_all_deps }
-
+        is_expected.to contain__haproxy('') 
     end
+
+    it { is_expected.to compile }
+    it { is_expected.to compile.with_all_deps }
 
 end
