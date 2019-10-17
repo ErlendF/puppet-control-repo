0789fbcd3e9ea9640f195b2f410f1d13fcf20e23
Aksel Baardsen <akselba@stud.ntnu.no>
2019-10-17 19:26:44 +0200

    reverting rspec_helper.rb for the n'th time

--- site-modules/profile/spec/spec_helper.rb
+++ site-modules/profile/spec/spec_helper.rb
@@ -5,33 +5,33 @@ require 'puppetlabs_spec_helper/module_spec_helper'
 
 #require 'rspec-puppet-facts'
 
-require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
+#require 'spec_helper_local' if File.file?(File.join(File.dirname(__FILE__), 'spec_helper_local.rb'))
 
-include RspecPuppetFacts
+#include RspecPuppetFacts
 
-default_facts = {
-  puppetversion: Puppet.version,
-  facterversion: Facter.version,
-}
+#default_facts = {
+#  puppetversion: Puppet.version,
+#  facterversion: Facter.version,
+#}
 
-RSpec.configure do |c|
-  c.before :each do
-    # set to strictest setting for testing
-    # by default Puppet runs at warning level
-    Puppet.settings[:strict] = :warning
-  end
-  c.filter_run_excluding(bolt: true) unless ENV['GEM_BOLT']
-  c.after(:suite) do
-  end
-end
+#RSpec.configure do |c|
+#  c.before :each do
+#    # set to strictest setting for testing
+#    # by default Puppet runs at warning level
+#    Puppet.settings[:strict] = :warning
+#  end
+#  c.filter_run_excluding(bolt: true) unless ENV['GEM_BOLT']
+#  c.after(:suite) do
+#  end
+#end
 
 # Ensures that a module is defined
 # @param module_name Name of the module
-def ensure_module_defined(module_name)
-  module_name.split('::').reduce(Object) do |last_module, next_module|
-    last_module.const_set(next_module, Module.new) unless last_module.const_defined?(next_module, false)
-    last_module.const_get(next_module, false)
-  end
-end
+#def ensure_module_defined(module_name)
+#  module_name.split('::').reduce(Object) do |last_module, next_module|
+#    last_module.const_set(next_module, Module.new) unless last_module.const_defined?(next_module, false)
+#    last_module.const_get(next_module, false)
+#  end
+#end
 
 # 'spec_overrides' from sync.yml will appear below this line
