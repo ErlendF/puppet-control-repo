adf135251413de3be7381dac696309950942c8b6
Aksel Baardsen <onion@Aksels-MacBook-Pro.local>
2019-10-18 12:38:55 +0200

    tests makes sure that the balancer contains frontend nodes & a listener

--- site-modules/profile/spec/classes/haproxy_spec.rb
+++ site-modules/profile/spec/classes/haproxy_spec.rb
@@ -8,9 +8,17 @@ describe 'profile::haproxy', :type => :class do
     } }
 
     it 'should include ::haproxy' do
-        is_expected.to contain_class('haproxy') 
-        #is_expected.to contain_haproxy('')
+        is_expected.to contain_class('haproxy')
     end
+
+    it 'should have only 4 classes (::haproxy, profile::haproxy, haproxy::xxx & xxx)' do 
+        is_expected.to have_class_count(4)
+    end
+
+    it { is_expected.to contain_haproxy__listen("balancer") }
+    it { is_expected.to contain_haproxy__balancermember("lin0") }
+    it { is_expected.to contain_haproxy__balancermember("lin1") }
+
 
     it { is_expected.to compile }
     it { is_expected.to compile.with_all_deps }
