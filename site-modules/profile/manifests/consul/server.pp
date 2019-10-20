954390d2599622fe17e15ca692f07fec61106421
Johan S <Johasel@stud.ntnu.no>
2019-10-20 08:07:06 +0200

    add consul ui

--- site-modules/profile/manifests/consul/server.pp
+++ site-modules/profile/manifests/consul/server.pp
@@ -10,6 +10,8 @@ class profile::consul::server {
   class { '::consul':
     version     => '1.6.0',
     config_hash => {
+      'client_addr'      => '0.0.0.0',  ## make ui reachable
+      'ui'               => true,       ## add ui
       'bootstrap_expect' => 3,
       'data_dir'         => '/opt/consul',
       'datacenter'       => 'NTNU',
