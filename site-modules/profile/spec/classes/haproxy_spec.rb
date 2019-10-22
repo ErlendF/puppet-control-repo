918675ef3623509c3a5f6ebd03e19ab60df562bb
Aksel Baardsen <onion@Aksels-MacBook-Pro.local>
2019-10-22 13:16:39 +0200

    Added necessary facts & deleted unecessary tests (not testing logic)

--- site-modules/profile/spec/classes/haproxy_spec.rb
+++ site-modules/profile/spec/classes/haproxy_spec.rb
@@ -5,23 +5,14 @@ describe 'profile::haproxy', :type => :class do
     let(:facts) { {
         :kernel => "Linux",
         :osfamily       => 'debian',
-        :os => { :family => 'debian' },
+        :os => { 
+            :family => 'debian',
+            :name   => 'Ubuntu' ,
+        },
         :ipaddress      => '127.0.0.1',
         :architecture   => 'amd64',
+        :path           => 'FIXME',
     } }
-
-    it 'should include ::haproxy' do
-        is_expected.to contain_class('haproxy')
-    end
-
-    it 'should have only 4 classes (::haproxy, profile::haproxy, haproxy::xxx & xxx)' do 
-        is_expected.to have_class_count(4)
-    end
-
-    it { is_expected.to contain_haproxy__listen("balancer") }
-    it { is_expected.to contain_haproxy__balancermember("lin0") }
-    it { is_expected.to contain_haproxy__balancermember("lin1") }
-
 
     it { is_expected.to compile }
     it { is_expected.to compile.with_all_deps }
