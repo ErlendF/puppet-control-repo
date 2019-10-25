10d2b10fa0da1315d96b9dcdec732ad1292e0026
Johan <johasel@stud.ntnu.no>
2019-10-26 01:17:49 +0200

    giving sensu another go

--- site-modules/profile/manifests/sensu/agent.pp
+++ site-modules/profile/manifests/sensu/agent.pp
@@ -1,26 +1,15 @@
 # profile::sensu::agent
 class profile::sensu::agent {
-#  class { 'sensu::agent':
-#    backends    => ['sensuback.service.consul:8081'],
-#    config_hash => {
-#      'subscriptions' => ['linux', 'apache-servers'],
-#    },
-#  }
-#
-#  sensu_check { 'check-cpu':
-#    ensure        => 'present',
-#    command       => 'check-cpu.sh -w 75 -c 90',
-#    interval      => 60,
-#    subscriptions => ['linux'],
-#  }
-#
-#  package { 'sensu-plugins-disk-checks':
-#    ensure   => 'latest',
-#    provider => sensu_gem,
-#  }
-#  class { 'sensu::plugins':
-#    plugins => {
-#    'disk-checks' => { 'version' => 'latest' },
-#    },
-#  }
+
+  class { 'sensu::agent':
+    backends    => ['sensuback.service.consul:8081'],
+    config_hash => {
+      'subscriptions' => ['linux', 'apache-servers'],
+    },
+  }
+
+  class { 'sensu::plugins':
+    plugins          => ['memory-checks', 'disk-checks'],
+    gem_dependencies => ['vmstat'],
+  }
 }
