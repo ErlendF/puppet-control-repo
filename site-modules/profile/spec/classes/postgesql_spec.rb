96b2677e3a3b875c147b1cb818f04dc56f51de16
Aksel Baardsen <onion@Aksels-MacBook-Pro.local>
2019-10-26 17:02:18 +0200

    Created basic unit test for profile::postgresql
    This unit test only ensures that the class compiles.
    Also erased a failing test (base_manager)

---
+++ site-modules/profile/spec/classes/postgesql_spec.rb
@@ -0,0 +1,33 @@
+require 'spec_helper'
+
+describe 'profile::postgresql', :type => :class do
+
+    test_on = {
+        :hardwaremodels => ['x86_64'],
+        :supported_os   => [
+            {
+                'operatingsystem'        => 'Ubuntu',
+                'operatingsystemrelease' => '18.04',
+            },
+        ],
+    }
+    
+    on_supported_os(test_on).each do |os, facts|
+        let(:facts) do
+            facts
+        end
+
+        let(:hiera_config) { 'spec/fixtures/hiera/hiera.yaml' }
+        hiera = Hiera.new({:config => 'spec/fixtures/hiera/hiera.yaml'})
+
+        #let(:hiera_data) {{
+        #    :'ssh_keys'
+        #}}
+
+
+        it { is_expected.to compile }
+
+
+    end
+
+end
