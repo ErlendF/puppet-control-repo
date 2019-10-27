0a1892ece21d1981d14f28f76be27496420c7e61
Aksel Baardsen <onion@Aksels-MacBook-Pro.local>
2019-10-27 07:35:38 +0100

    added basic unit-test to dns client

---
+++ site-modules/profile/spec/classes/dns/client_spec.rb
@@ -0,0 +1,28 @@
+require 'spec_helper'
+
+describe 'profile::dns::client' do
+
+    test_on = {
+        :hardwaremodels => ['x86_64'],
+        :supported_os   => [
+            {
+                'operatingsystem'        => 'Ubuntu',
+                'operatingsystemrelease' => '18.04',
+            },
+        ],
+    }
+    
+    on_supported_os(test_on).each do |os, facts|
+        let(:facts) do
+            facts
+        end
+
+        let(:hiera_config) { 'spec/fixtures/hiera/hiera.yaml' }
+        hiera = Hiera.new({:config => 'spec/fixtures/hiera/hiera.yaml'})
+
+        it { is_expected.to compile }
+
+
+    end
+
+end
