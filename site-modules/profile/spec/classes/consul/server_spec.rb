3da98c3bd4e16da8e5d9ba52a99b1f07eb9161e6
Aksel Baardsen <onion@Aksels-MacBook-Pro.local>
2019-10-27 14:54:42 +0100

    Added compile test for consul::server

---
+++ site-modules/profile/spec/classes/consul/server_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe 'profile::consul::server' do
+
+    test_on = {
+        :hardwaremodels => ['x86_64'],
+        :supported_os   => [
+            {
+                'operatingsystem'        => 'Ubuntu',
+                'operatingsystemrelease' => '18.04',
+            },
+        ],
+    }
+    
+    on_supported_os(test_on).each do |os, facts|
+
+        let(:facts) do
+            facts
+            {'serverip' => '127.0.0.1', 'architecture' => 'x86_64', 'os' => 
+                {'family' => 'Linux', 'name' => 'Ubuntu'}, 'kernel' => 'linux', 'path' => './'}
+        end
+
+        let(:hiera_config) { 'spec/fixtures/hiera/hiera.yaml' }
+        hiera = Hiera.new({:config => 'spec/fixtures/hiera/hiera.yaml'})
+        
+
+        it { is_expected.to compile }
+
+
+    end
+
+end
