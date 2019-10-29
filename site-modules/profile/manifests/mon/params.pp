7a3348eec39e278f2911272230edf4057b78a31d
Johan <johasel@stud.ntnu.no>
2019-10-29 18:51:19 +0100

    param fix

--- site-modules/profile/manifests/mon/params.pp
+++ site-modules/profile/manifests/mon/params.pp
@@ -1,6 +1,6 @@
 class profile::mon::params (
-  String $influxdb_password,
-  String $grafana_password,
+  String $influxdb_password = lookup('mon::params::influxdb_password'),
+  String $grafana_password = lookup('mon::params::grafana_password'),
   String $influxdb_database = 'bolt',
   String $influxdb_user = 'bolt',
   String $grafana_url = 'http://localhost:8080',
