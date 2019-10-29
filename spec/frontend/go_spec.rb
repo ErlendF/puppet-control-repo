cfbf2d688c126758ef748e1cfeb115b30075ea32
Aksel Baardsen <onion@Aksels-MacBook-Pro.local>
2019-10-29 14:40:12 +0100

    Added acceptance test for frontend servers.
    The test checks if go is up and running with
    correct version. Also checks that the service
    'web' (the api) is up and running. Also makes
    sure that correct port is open and listening.

---
+++ spec/frontend/go_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe file('/usr/local/go') do
+    it { should exist }
+    it { should be_directory }
+end
+
+describe command('/usr/local/go/bin/go version') do 
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match 'go version go1.13.3' }
+end
+
+describe service('web') do
+    it { should be_running }
+end
+
+describe service('consul') do
+    it { should be_running }
+end
+
+describe file('/root/webserverRepo') do
+    it { should be_directory }
+end
+
+describe port(80) do
+    it { should be_listening }
+end
+
+describe port(443) do
+    it { should_not be_listening }
+end
