652df6c943fe893ad01f6e0a1d71f807c117a53c
Johan <johasel@stud.ntnu.no>
2019-10-30 21:21:04 +0100

    webmon test

--- site-modules/profile/manifests/mon/webmon.pp
+++ site-modules/profile/manifests/mon/webmon.pp
@@ -3,12 +3,12 @@
 #
 class profile::mon::webmon () inherits profile::mon::telegraf {
   $nginx_servers = lookup('nginx::servers')
-  $servers_arr = Array[String]
-  $nginx_servers.each |Hash $server| {
-    $server.each |String $name, String $address| {
-      servers_arr << $address
-    }
-  }
+  # $servers_arr = Array[String]
+  # $nginx_servers.each |Hash $server| {
+  #   $server.each |String $name, String $address| {
+  #     servers_arr << $address
+  #   }
+  # }
 
   telegraf::input { 'http_check':
     plugin_type => 'http_response',
@@ -16,14 +16,12 @@ class profile::mon::webmon () inherits profile::mon::telegraf {
       {
         'method'           => 'GET',
         'follow_redirects' => true,
-        'urls'             => $servers_arr,
-        # 'http://puppetdb.service.consul:5000'
-      #[
-        # $nginx_servers['puppetdb.iacprosjekt.ntnu'],
-        # $nginx_servers['consul.iacprosjekt.ntnu'],
-        # $nginx_servers['iacprosjekt.ntnu'],
-        # $nginx_servers['grafana.iacprosjekt.ntnu']
-      #],
+        'urls'             => [
+        $nginx_servers['puppetdb.iacprosjekt.ntnu'],
+        $nginx_servers['consul.iacprosjekt.ntnu'],
+        $nginx_servers['iacprosjekt.ntnu'],
+        $nginx_servers['grafana.iacprosjekt.ntnu']
+      ],
     },
     ]
   }
