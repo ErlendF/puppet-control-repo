94b0d2cab9219315495c6cc71d1be0b756d64099
Johan <johasel@stud.ntnu.no>
2019-10-31 10:08:12 +0100

    Revert "tmp fix"
    This reverts commit 6805b179271e5d0939aa51b6a32f43ac6560e47b.

--- site-modules/role/manifests/manager.pp
+++ site-modules/role/manifests/manager.pp
@@ -2,10 +2,10 @@
 # role::manager
 #
 class role::manager {
+  include ::profile::base_linux
+  include ::profile::base_manager
   include ::profile::dns::client
   include ::profile::dns::server
   include ::profile::consul::server
-  include ::profile::base_linux
-  Class['::profile::base_manager'] ->
-  Class['::profile::mon::telegraf']
+  include ::profile::mon::telegraf
 }
